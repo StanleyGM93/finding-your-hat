10745e14cbe22064391a314996e735a59e082b8f
Stanley Mair <stanleygmair@gmail.com>
2022-11-08 05:07:28 +1300

    Created functions for each user input

--- main.js
+++ main.js
@@ -8,19 +8,63 @@ const pathCharacter = "*";
 class Field {
 	constructor(field) {
 		this.field = field;
-		let horizontalLocation = 0
-		let verticalLocation = 0
-		let currentLocation = field[verticalLocation][horizontalLocation]
-		let isGameOver = false;
+		let horizontalLocation = 0;
+		let verticalLocation = 0;
+		let currentLocation = field[verticalLocation][horizontalLocation];
+		let outerBoundVertical = verticalLocation.length;
+		let outerBoundHorizontal = horizontalLocation.length;
+		let isGameCurrent = true;
 	}
 	print() {
 		return this.field.join("\n");
 	}
 	moveResult(character) {
 		if (character === hat) {
-			return "You have won the game"
+			console.log("You have won the game");
+			return isGameCurrent === false;
 		} else if (character === hole) {
-			return "Oh no! You've fallen into a hole"
+			console.log("Oh no! You've fallen into a hole");
+			return isGameCurrent === false;
+		} else if (character === fieldCharacter) {
+			myField.currentLocation = pathCharacter;
+		} else {
+			console.log("How did we end up here???");
+		}
+	}
+	moveRight() {
+		if (horizontalLocation <= outerBoundHorizontal) {
+			horizontalLocation++;
+			this.moveResult(this.field.currentLocation);
+		} else {
+			console.log("You've fallen out of bounds");
+			return isGameCurrent === false;
+		}
+	}
+	moveLeft() {
+		if (horizontalLocation >= 0) {
+			horizontalLocation--;
+			this.moveResult(this.field.currentLocation);
+		} else {
+			console.log("You've fallen out of bounds");
+			return isGameCurrent === false;
+		}
+	}
+	moveUp() {
+		if (verticalLocation >= 0) {
+			verticalLocation--;
+			this.moveResult(this.field.currentLocation);
+		} else {
+			console.log("You've fallen out of bounds");
+			return isGameCurrent === false;
+		}
+	}
+	moveDown() {
+		if (verticalLocation <= outerBoundVertical) {
+			verticalLocation++;
+			this.moveResult(this.field.currentLocation);
+		} else {
+			console.log("You've fallen out of bounds");
+			return isGameCurrent === false;
 		}
 	}
 }
@@ -31,8 +75,18 @@ const myField = new Field([
 	["░", "^", "░"],
 ]);
 
-do 
-//Game stuff in here
-while (isGameOver); 
+const playGame = () => {
+	do {
+		myField.print();
 
+		let userInput = prompt("Which way?");
+		if (userInput === "r" || "R" || "right" || "Right") {
+			moveRight();
+		} else {
+			console.log("This isn`t working :(");
+		}
+	} while (isGameCurrent);
+};
+
+playGame();
 console.log(myField.print());
