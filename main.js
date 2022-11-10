17b6b386755a0f9bfb2c633dac97a5d91070442f
Stanley Mair <stanleygmair@gmail.com>
2022-11-11 05:11:38 +1300

    Move functions are working. userinput via prompt needs fixing

--- main.js
+++ main.js
@@ -4,17 +4,15 @@ const hat = "^";
 const hole = "O";
 const fieldCharacter = "░";
 const pathCharacter = "*";
+let isGameCurrent = true;
 
 class Field {
 	constructor(field) {
 		this.field = field;
 		this.horizontalLocation = 0;
 		this.verticalLocation = 0;
-		this.outerBoundVertical = this.field.length;
-		this.outerBoundHorizontal = this.field[0].length;
 		this.currentLocation =
 			this.field[this.verticalLocation][this.horizontalLocation];
-		this.isGameCurrent = true;
 	}
 
 	print() {
@@ -24,57 +22,59 @@ class Field {
 		return this.field[this.verticalLocation][this.horizontalLocation];
 	}
 	moveResult(character) {
-		console.log("character = " + character);
 		if (character === hat) {
 			console.log("You have won the game");
-			return (this.isGameCurrent = false);
+			return (isGameCurrent = false);
 		} else if (character === hole) {
 			console.log("Oh no! You've fallen into a hole");
-			return (this.isGameCurrent = false);
+			return (isGameCurrent = false);
 		} else if (character === fieldCharacter) {
-			console.log("You've made it to the path character");
 			this.field[this.verticalLocation][this.horizontalLocation] =
 				pathCharacter;
-			return (this.isGameCurrent = true);
 		} else {
 			console.log("How did we end up here???");
 		}
 	}
 	moveRight() {
-		if (this.horizontalLocation <= this.outerBoundHorizontal) {
-			this.horizontalLocation++;
-
+		this.horizontalLocation++;
+		let outerBoundHorizontal = this.field[0].length;
+		if (this.horizontalLocation < outerBoundHorizontal) {
 			this.moveResult(this.printCharacter());
 		} else {
 			console.log("You've fallen out of bounds");
-			return this.isGameCurrent === false;
+			isGameCurrent = false;
+			return isGameCurrent;
 		}
 	}
 	moveLeft() {
-		if (horizontalLocation >= 0) {
-			horizontalLocation--;
-			this.moveResult(this.field.currentLocation);
+		this.horizontalLocation--;
+		if (this.horizontalLocation >= 0) {
+			this.moveResult(this.printCharacter());
 		} else {
 			console.log("You've fallen out of bounds");
-			return isGameCurrent === false;
+			isGameCurrent = false;
+			return isGameCurrent;
 		}
 	}
 	moveUp() {
-		if (verticalLocation >= 0) {
-			verticalLocation--;
-			this.moveResult(this.field.currentLocation);
+		this.verticalLocation--;
+		if (this.verticalLocation >= 0) {
+			this.moveResult(this.printCharacter());
 		} else {
 			console.log("You've fallen out of bounds");
-			return isGameCurrent === false;
+			isGameCurrent = false;
+			return isGameCurrent;
 		}
 	}
 	moveDown() {
-		if (verticalLocation <= outerBoundVertical) {
-			verticalLocation++;
-			this.moveResult(this.field.currentLocation);
+		verticalLocation++;
+		let outerBoundVertical = this.field.length;
+		if (verticalLocation < outerBoundVertical) {
+			this.moveResult(this.printCharacter());
 		} else {
 			console.log("You've fallen out of bounds");
-			return isGameCurrent === false;
+			isGameCurrent = false;
+			return isGameCurrent;
 		}
 	}
 }
@@ -89,14 +89,16 @@ const playGame = () => {
 	do {
 		myField.print();
 
-		let userInput = prompt("Which way?");
+		const userInput = prompt("Which way? ");
+		console.log(userInput);
 		if (userInput === "r" || "R" || "right" || "Right") {
 			myField.moveRight();
+		} else if (userInput === "l" || "L" || "left" || "Left") {
+			myField.moveLeft();
 		} else {
 			console.log("This isn`t working :(");
 		}
-		console.log(`this.isGameCurrent = ${this.isGameCurrent}`);
-	} while (this.isGameCurrent);
+	} while (isGameCurrent);
 };
 
 playGame();
